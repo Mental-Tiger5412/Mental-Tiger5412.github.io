43e531ec6f4b720ac244e7e13ff0cffbb190f8d3
Mental-Tiger5412 <75413686+Mental-Tiger5412@users.noreply.github.com>
2020-12-03 02:41:38 -0600

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,15 @@
+function LeadershipTheory() {
+   document.getElementById("Portfolio").src="Leadership_Theory_Paper.pdf"
+   }
+
+function Southwest() {
+   document.getElementById("Portfolio").src="Southwest_Airlines.pdf"
+   }
+
+function Literature() {
+   document.getElementById("Portfolio").src="Literature.pdf"
+   }
+
+function Hero() {
+   document.getElementById("Portfolio").src="Hero_Literature.pdf"
+   }
